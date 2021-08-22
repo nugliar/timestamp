593bcaaba1854630002b49023a2168a52c44ba94
nugliar <hashan.hall@gmail.com>
2021-08-22 14:05:12 -0500

    minor changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,12 +19,25 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:time", function(req, res) {
+  let time = req.params.time
 
+  if (!/[^0-9]/.test(time)) {
+    time = parseInt(time)
+  }
+
+  const date = new Date(time)
+
+  res.json({
+    unix: Date.parse(date),
+    utc: date.toUTCString()
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
